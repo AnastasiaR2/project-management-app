e048e1f97301252fe9ba379af8435a7423c13684
AnastasiaR2 <anastasiia.romaniuk007@gmail.com>
2025-04-03 17:34:33 +0300

    Create DataTable component

---
+++ src/components/DataTable.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { defineProps } from "vue";
+
+interface Column {
+  key: string;
+  label: string;
+}
+
+defineProps<{
+  data: Record<string, unknown>[];
+  columns: Column[];
+}>();
+</script>
+
+<template>
+  <div>
+    <table>
+      <thead>
+        <tr>
+          <th v-for="(column, i) in columns" :key="`${column.key}-${i}`">
+            {{ column.label }}
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in data" :key="item.id as string">
+          <td v-for="(column, i) in columns" :key="`${column.key}${i}`">
+            {{ item[column.key] }}
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,35 @@
 <script setup lang="ts">
-import TheWelcome from "../components/TheWelcome.vue";
+import DataTable from "@/components/DataTable.vue";
+const projects = [
+  {
+    id: "1",
+    name: "Project 1",
+    description: "Description of Project 1",
+    status: "In Progress",
+    tasksCount: 5,
+    createdAt: "2025-04-10",
+  },
+  {
+    id: "2",
+    name: "Project 2",
+    description: "Description of Project 2",
+    status: "Completed",
+    tasksCount: 3,
+    createdAt: "2025-05-10",
+  },
+];
+const columns = [
+  { key: "id", label: "ID" },
+  { key: "name", label: "Name" },
+  { key: "description", label: "Description" },
+  { key: "status", label: "Status" },
+  { key: "tasksCount", label: "Tasks Count" },
+  { key: "createdAt", label: "Created At" },
+];
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <DataTable :columns="columns" :data="projects"> </DataTable>
   </main>
 </template>
