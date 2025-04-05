54c9dafa43690226e4d4ec7683f15b092466c58d
AnastasiaR2 <anastasiia.romaniuk007@gmail.com>
2025-04-05 21:00:16 +0300

    Implement functionality to delete project

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,17 +6,21 @@ import AppButton from "@/components/AppButton.vue";
 import ProjectForm from "@/features/projects/ProjectForm.vue";
 import type { Project } from "@/services/projects/types";
 import { useProjectStore } from "@/stores/projects";
+import { useTaskStore } from "@/stores/tasks";
 
-const isModalOpen = ref(false);
+const isAddOrEditModalOpen = ref(false);
+const isDeleteModalOpen = ref(false);
 const currentProject = ref({} as Project);
+const projectIdToDelete = ref("");
 
 const projectStore = useProjectStore();
+const taskStore = useTaskStore();
 
 async function handleAddProject(project: Project) {
   const response = await projectStore.dispatchCreateProject(project);
 
   if (response?.status === 201) {
-    isModalOpen.value = false;
+    isAddOrEditModalOpen.value = false;
   } else {
     console.error("Failed to create project:", response?.status);
   }
@@ -26,7 +30,23 @@ async function handleUpdateProject(project: Project) {
   const response = await projectStore.dispatchUpdateProject(project);
 
   if (response?.status === 200) {
-    isModalOpen.value = false;
+    isAddOrEditModalOpen.value = false;
+  } else {
+    console.error("Failed to update project:", response?.status);
+  }
+}
+
+async function handleDeleteProject(id: string) {
+  const response = await projectStore.dispatchDeleteProject(id);
+
+  if (response?.status === 200) {
+    isDeleteModalOpen.value = false;
+    const projectTasks = taskStore.getTasksByProjectId(id);
+    const projectTaskIds = projectTasks.value.map((task) => task.id);
+
+    for (const taskId of projectTaskIds) {
+      await taskStore.dispatchDeleteTask(taskId);
+    }
   } else {
     console.error("Failed to update project:", response?.status);
   }
@@ -34,7 +54,12 @@ async function handleUpdateProject(project: Project) {
 
 function handleEditProject(project: Project) {
   currentProject.value = project;
-  isModalOpen.value = true;
+  isAddOrEditModalOpen.value = true;
+}
+
+function handleDeleteModalOpen(id: string) {
+  projectIdToDelete.value = id;
+  isDeleteModalOpen.value = true;
 }
 
 function submitProject(project: Project) {
@@ -50,13 +75,34 @@ function submitProject(project: Project) {
   <main>
     <div class="main-title-wrapper">
       <h1>Projects</h1>
-      <AppButton @click="isModalOpen = true">Add Project</AppButton>
+      <AppButton @click="isAddOrEditModalOpen = true">Add Project</AppButton>
     </div>
-    <AppModal v-if="isModalOpen" @close="isModalOpen = false">
+    <AppModal v-if="isAddOrEditModalOpen" @close="isAddOrEditModalOpen = false">
       <template #body>
         <ProjectForm @submit="submitProject" :project="currentProject" />
       </template>
     </AppModal>
-    <ProjectsTable @edit-btn-clicked="handleEditProject" />
+    <AppModal v-if="isDeleteModalOpen" @close="isDeleteModalOpen = false">
+      <template #title> Are you sure you want to delete this project? </template>
+      <template #body>
+        <p>All associated tasks will be removed as well.</p>
+        <div class="buttons-wrapper">
+          <AppButton @click="isDeleteModalOpen = false">Cancel</AppButton>
+          <AppButton @click="handleDeleteProject(projectIdToDelete)">Delete</AppButton>
+        </div>
+      </template>
+    </AppModal>
+    <ProjectsTable
+      @edit-btn-clicked="handleEditProject"
+      @delete-btn-clicked="handleDeleteModalOpen"
+    />
   </main>
 </template>
+
+<style scoped lang="scss">
+.buttons-wrapper {
+  display: flex;
+  gap: 10px;
+  margin-top: 20px;
+}
+</style>
